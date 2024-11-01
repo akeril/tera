f5ca2da9f16512dcf03cb6a4c8cdfdcb3c8f2e46
akeril <185402721+akeril@users.noreply.github.com>
2024-11-02 01:50:05 +0530

    refactor server impl

--- runner.go
+++ runner.go
@@ -1,40 +1,30 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"strconv"
-)
-
 type Runner struct {
 	cfg     Config
 	watcher Watcher
-	server  *http.Server
+	server  Server
 }
 
 func NewRunnerWithConfig(cfg Config) (*Runner, error) {
-	server := http.Server{
-		Addr: ":" + strconv.Itoa(cfg.Port),
-	}
 	watcher, err := NewWatcher()
 	if err != nil {
 		return nil, err
 	}
 	watcher.Add(cfg.WatchDir)
 
+	server := NewServer(cfg.Port, cfg.WatchDir)
 	return &Runner{
 		cfg:     cfg,
 		watcher: watcher,
-		server:  &server,
+		server:  server,
 	}, nil
 }
 
 func (r *Runner) Run() {
-	log.Println("Listening on port", r.cfg.Port)
-	http.Handle("GET /", http.FileServer(http.Dir(r.cfg.WatchDir)))
-
+	// watch fs for changes
 	go r.watcher.Watch()
-	if err := r.server.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
+
+	// serve http requests
+	r.server.Serve()
 }
