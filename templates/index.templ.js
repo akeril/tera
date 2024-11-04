6b509abdf491f2fdbef1da3cd3e95774279c32af
akeril <185402721+akeril@users.noreply.github.com>
2024-11-05 00:19:38 +0530

    refactor template files to be modular

---
+++ templates/index.templ.js
@@ -0,0 +1,55 @@
+// {{define "script"}}
+
+// add websocket handlers
+const ws = new WebSocket("{{.Uri}}");
+
+ws.addEventListener("open", () => {
+  console.log("Websocket connection established!");
+});
+
+ws.addEventListener("close", () => {
+  console.log("Websocket connection terminated!");
+  const html = document.querySelector("html");
+  html.innerHTML = "<p> Connection terminated!</p>";
+});
+
+ws.addEventListener("message", async (e) => {
+  const event = JSON.parse(e.data);
+  reload(event);
+});
+
+function reload(event) {
+  // strip url prefix
+  const url = event.Name.substr(2);
+
+  // if entrypoint changes, reload the entire page
+  if (url == "{{.Entrypoint}}") {
+    loadEntryPoint();
+    return;
+  }
+
+  for (let tag of ["href", "src", "data"]) {
+    renderUpdates(tag, url);
+  }
+}
+
+// locate changed elements and update them
+function renderUpdates(tag, url) {
+  const elements = document.querySelectorAll(`[${tag}*="${url}"]`);
+  for (let element of elements) {
+    const newUrl = `${url}?t=${new Date().getTime()}`;
+    element.setAttribute(tag, newUrl);
+  }
+}
+
+// initial data fetch
+async function loadEntryPoint() {
+  const resp = await fetch("{{.Entrypoint}}");
+  const data = await resp.text();
+  const html = document.querySelector("html");
+  html.innerHTML = data;
+}
+
+loadEntryPoint();
+
+// {{end}}
