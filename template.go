411c3bdfc3f69f16fe48e9443c199afc58ec9f12
kjabin <185402721+kjabin@users.noreply.github.com>
2024-11-05 00:19:38 +0530

    refactor template files to be modular

--- template.go
+++ template.go
@@ -13,15 +13,21 @@ var fs embed.FS
 type TemplConfig struct {
 	Uri        string
 	Entrypoint string
+	Script     []byte
 }
 
 func generateTemplate(cfg TemplConfig) (io.ReadWriter, error) {
-	templ, err := template.ParseFS(fs, "templates/templ.html")
+	wr := new(bytes.Buffer)
+	templ, err := template.ParseFS(fs, "templates/index.templ.html")
 	if err != nil {
 		return nil, err
 	}
 
-	wr := new(bytes.Buffer)
+	templ, err = templ.ParseFS(fs, "templates/index.templ.js")
+	if err != nil {
+		return nil, err
+	}
+
 	err = templ.Execute(wr, cfg)
 	if err != nil {
 		return nil, err
