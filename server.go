5521d51d312fc03d4acc1013551d7f575dde97d0
kjabin <185402721+kjabin@users.noreply.github.com>
2024-11-09 20:53:55 +0530

    add pdf file support

--- server.go
+++ server.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 	"strconv"
@@ -34,27 +33,37 @@ func NewServer(port int, watchDir string, entrypoint string) Server {
 		entrypoint: entrypoint,
 		watchDir:   watchDir,
 	}
-	http.HandleFunc("GET /", s.handleRouting)
-	http.HandleFunc("GET /tera", s.LiveReload)
+	http.HandleFunc("GET /", s.Router)
 	return s
 }
 
-func (s Server) handleFS(w http.ResponseWriter, r *http.Request) {
-	http.FileServer(http.Dir(s.watchDir)).ServeHTTP(w, r)
+func (s Server) Serve() {
+	log.Printf("Listening on http://localhost:%d", s.port)
+	if err := s.server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
 
-// handle default screen for tera
-func (s Server) handleEntryPoint(w http.ResponseWriter, r *http.Request) {
-	data, err := generateIndex(TemplConfig{Entrypoint: s.entrypoint})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
+// multiplex on websocket and http requests
+func (s Server) Router(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Upgrade") == "websocket" {
+		s.handleWS(w, r)
 		return
 	}
-	w.Write(data)
+	if r.URL.Path == "/" {
+		s.handleIndex(w, r)
+		return
+	}
+	if r.URL.Path == "/tera" {
+		s.handleReloader(w, r)
+		return
+	}
+	s.handleFS(w, r)
 }
 
-func (s Server) LiveReload(w http.ResponseWriter, r *http.Request) {
-	data, err := generateTemplate(TemplConfig{
+// returns index page for tera with scripts injected
+func (s Server) handleIndex(w http.ResponseWriter, _ *http.Request) {
+	data, err := ParseEntryPoint(TemplConfig{
 		Uri:        fmt.Sprintf("ws://localhost:%v", s.port),
 		Entrypoint: s.entrypoint,
 	})
@@ -62,8 +71,27 @@ func (s Server) LiveReload(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
+	w.Write(data)
+}
+
+// returns javascript file for client side live reloading
+func (s Server) handleReloader(w http.ResponseWriter, _ *http.Request) {
+	data, err := ParseTemplate("templates/tera.js", TemplConfig{
+		Uri:        fmt.Sprintf("ws://localhost:%v", s.port),
+		Entrypoint: s.entrypoint,
+	})
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Header().Add("Content-Type", "text/javascript; charset=utf-8")
-	io.Copy(w, data)
+	w.Write(data)
+}
+
+// file server handler
+func (s Server) handleFS(w http.ResponseWriter, r *http.Request) {
+	http.FileServer(http.Dir(s.watchDir)).ServeHTTP(w, r)
 }
 
 // handles incoming websocket requests
@@ -97,25 +125,7 @@ func (s Server) handleWS(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (s Server) handleRouting(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Upgrade") == "websocket" {
-		s.handleWS(w, r)
-		return
-	}
-	if r.URL.Path == "/" {
-		s.handleEntryPoint(w, r)
-		return
-	}
-	s.handleFS(w, r)
-}
-
-func (s Server) Serve() {
-	log.Printf("Listening on http://localhost:%d", s.port)
-	if err := s.server.ListenAndServe(); err != nil {
-		log.Fatal(err)
-	}
-}
-
+// invoke on separate thread to broadcast events
 func (s Server) BroadcastEvents(ch chan fsnotify.Event) {
 	for event := range ch {
 		log.Println(event)
